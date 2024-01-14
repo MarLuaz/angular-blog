f43c9d4d2ad3a90fe254510dd163fa37424fe135
Maria Luíza <marialuizasilvabrito@gmail.com>
2024-01-13 21:53:26 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"Nova versão do Angular",
+        "description":"Olá mundo!",
+        "photoCover":"https://media.licdn.com/dms/image/C4D12AQFksha8XMfv1Q/article-inline_image-shrink_1000_1488/0/1643887931253?e=1709769600&v=beta&t=tMsFpviH7UGELAC99nloD37VhLfD-QVnU2OMzIQJc-k"
+    },
+    {
+        "id":"2",
+        "title":"Versões do Angular",
+        "description":"Venha conhecer!",
+        "photoCover":"https://www.c-metric.com/wp-content/uploads/2022/04/11-Angular-Versions-1.jpg"
+    }
+]
